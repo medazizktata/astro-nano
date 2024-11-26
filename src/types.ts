4e32c19ac7d924025741ca8eea58ce62e1a11278
medazizktata <aziiizktata7@gmail.com>
2024-11-26 02:59:14 +0100

    LinkedIn Github type match

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,10 @@
 export type Site = {
   NAME: string;
   EMAIL: string;
+  LINKEDIN: string;
+  GITHUB: string;
   NUM_POSTS_ON_HOMEPAGE: number;
+  NUM_EDU_ON_HOMEPAGE: number;
   NUM_WORKS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
 };
